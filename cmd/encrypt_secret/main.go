a460fcf693eaa3d96b1efdbd5378e3bbd6d4fceb
Cory Redmond <cory.redmond@nandos.co.uk>
2022-09-06 12:34:20 +0100

    Allow decrypting secrets

--- cmd/encrypt_secret/main.go
+++ cmd/encrypt_secret/main.go
@@ -9,23 +9,37 @@ import (
 	"github.com/hashicorp/terraform-provider-scaffolding/encryption"
 )
 
+var privateKey string
 var publicKey string
 var input string
+var decrypt bool
 
 func main() {
 	flag.StringVar(&publicKey, "public-key", os.Getenv("SECRET_PUBLIC_KEY"), "The public key to encrypt against")
 	flag.StringVar(&input, "input", "", "The secret to encrypt")
+
+	flag.StringVar(&privateKey, "private-key", os.Getenv("SECRET_PRIVATE_KEY"), "The private key to decrypt with")
+	flag.BoolVar(&decrypt, "decrypt", false, "Decrypt")
+
 	flag.Parse()
 
-	if publicKey == "" {
+	if !decrypt && publicKey == "" {
+		privateKey = ""
 		_, _ = fmt.Fprintf(os.Stderr, "The public key was not supplied, set the environment variable SECRET_PUBLIC_KEY or use the -public-key flag\n")
 		os.Exit(1)
 		return
 	}
 
-	enc, err := encryption.New(publicKey, "")
+	if decrypt && privateKey == "" {
+		publicKey = ""
+		_, _ = fmt.Fprintf(os.Stderr, "The private key was not supplied, set the environment variable SECRET_PRIVATE_KEY or use the -private-key flag\n")
+		os.Exit(1)
+		return
+	}
+
+	enc, err := encryption.New(publicKey, privateKey)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error occurred setting up the encryptor: %v\n", err)
+		_, _ = fmt.Fprintf(os.Stderr, "Error occurred setting up the encryptor: %v\n", err)
 		os.Exit(1)
 		return
 	}
@@ -41,13 +55,19 @@ func main() {
 		input = string(data)
 	}
 
-	// fmt.Printf("%q\n", input)
-
 	if input == "" {
 		_, _ = fmt.Fprintf(os.Stderr, "[WARNING] The input going to be encrypted is an empty string.\n", err)
 	}
 
-	out, err := enc.EncryptString(input)
+	encFunc := enc.EncryptString
+	if decrypt {
+		encFunc = func(in string) (string, error) {
+			out, _, err := enc.DecryptString(in)
+			return out, err
+		}
+	}
+
+	out, err := encFunc(input)
 	if err != nil {
 		_, _ = fmt.Fprintf(os.Stderr, "Error occurred encrypting secret: %v\n", err)
 		os.Exit(1)
